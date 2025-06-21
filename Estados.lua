9118cd1cdfe50a2a544780cd46b1e84dd7230fac
Polar-script <soareswanderson667@gmail.com>
2025-06-21 10:47:10 -0300

    Create Estados.lua

---
+++ Estados.lua
@@ -0,0 +1,10 @@
+-- Estados possíveis da IA
+
+return {
+    Idle = "Idle",
+    Perseguindo = "Perseguindo",
+    Combatendo = "Combatendo",
+    Recuando = "Recuando",
+    Atolado = "Atolado",
+    Patrulhando = "Patrulhando"
+}
